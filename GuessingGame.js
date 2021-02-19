08739efd051b4664564ac7ee60046143b2520cb4
RenShadecrest <michaelaaronphoto@gmail.com>
2021-02-19 15:44:38 -0800

    Initial Upload

---
+++ GuessingGame.js
@@ -0,0 +1,52 @@
+var RNGresult;
+let score = 0;
+let misses = 0;
+var reveal = '? -- ? -- ?';
+var title = 'Can you guess the button I have in mind?';
+
+document.getElementById('revealResults').innerHTML = reveal;
+
+function RNG() {
+    RNGresult = Math.floor(Math.random() * Math.floor(3));
+    console.log(RNGresult);
+} 
+
+function gameButton(selection) {
+    RNG();
+    if (selection === RNGresult) {
+        title = 'You guessed right!';
+        score = score + 1;
+        updatePlay();
+    } else {
+        title = 'Better luck next time!';
+        misses = misses + 1;
+        updatePlay();
+    };
+};
+
+function results() {
+    if (RNGresult === 0) {
+        reveal = 'X -- O -- O';
+    } else if (RNGresult === 1) {
+        reveal = 'O -- X -- O';
+    } else {
+        reveal = 'O -- O -- X';
+    }
+}
+
+function updatePlay() {
+    results();
+    var scoreboard = `Your score is ${score} out of ${misses}!`;
+    document.getElementById('title').innerHTML = title;
+    document.getElementById('scoreboard').innerHTML = scoreboard;
+    document.getElementById('revealResults').innerHTML = reveal;
+}
+
+function reset() {
+    score = 0;
+    misses = 0;
+    updatePlay();
+};
+
+document.getElementById('title').innerHTML = title;
+document.getElementById('scoreboard').innerHTML = 'Take your best shot!';
